c327b05f18e664196dc4f0bfcd1dfe0d187a7ea8
Alekiss <alekiss.melo@dce.ufpb.br>
2021-04-22 16:29:30 -0300

    Atividade 05 - Inicio

---
+++ src/main/java/br/ufpb/dcx/aps/atividades/atv05/Campo.java
@@ -0,0 +1,16 @@
+package br.ufpb.dcx.aps.atividades.atv05;
+
+public class Campo {
+    private String id;
+    private String nome;
+
+    public Campo(String id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Campo(String id) {
+        this.id = id;
+    }
+
+}
---
+++ src/main/java/br/ufpb/dcx/aps/atividades/atv05/Formulario.java
@@ -0,0 +1,14 @@
+package br.ufpb.dcx.aps.atividades.atv05;
+
+public class Formulario {
+    private String titulo;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+}
